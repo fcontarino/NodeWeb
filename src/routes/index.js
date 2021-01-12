63a5c8174a7fed0a23c36e0d234399074e51aee8
fcontarino <facontarino@gmail.com>
2021-01-12 15:30:17 -0300

    node web

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,6 @@ router.get('/',(req,res) => {
     res.render('index.html',{title:'Node Web'})
 });
 
-router.get('/about',(req,res) => {
-    res.render('about.html', {title : 'Acerca de'})
-});
-
 router.get('/contact',(req,res) => {
     res.render('contact.html', {title : 'Contacto'})
 });
